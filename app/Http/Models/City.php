f9240d382c35701002cf4b18600343cc725daa3f
geraldchan1 <gerald@zl-my.com>
2016-11-21 21:50:25 +0800

    False timestamps and add fillable.

--- app/Http/Models/City.php
+++ app/Http/Models/City.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    protected $guarded = [];
+    public $timestamps = false;
+
+    protected $fillable = ['name'];
 
     public function hospitals(){
         return $this->hasMany('App\Http\Models\Hospital');
