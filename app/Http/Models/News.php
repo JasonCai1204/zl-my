b027043c9ee73f73802346f72c24f3d3238cf276
geraldchan1 <gerald@zl-my.com>
2016-11-25 18:25:47 +0800

    add published_at to $dates

--- app/Http/Models/News.php
+++ app/Http/Models/News.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class News extends Model
 {
     use SoftDeletes;
+    protected $dates = ['deleted_at', 'published_at'];
 
-    protected $dates = ['deleted_at'];
-
-    protected $guarded = [];
+    protected $fillable = ['title', 'cover_image', 'content', 'published_at'];
 }
