7c7b385deda970dddf5958da680ec383c7f38512
Jason Cai <jasoncai@hellohippies.com>
2016-11-26 15:41:45 +0800

    update masters_table

--- database/migrations/2016_11_04_034638_create_masters_table.php
+++ database/migrations/2016_11_04_034638_create_masters_table.php
@@ -14,7 +14,7 @@ class CreateMastersTable extends Migration
     public function up()
     {
         Schema::create('masters', function (Blueprint $table) {
-            $table->string('id',2);
+            $table->string('id',6);
             $table->string('department_id',4);
             $table->string('name',5);
             $table->string('phone_number',11)->unique();
