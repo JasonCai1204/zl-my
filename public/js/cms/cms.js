459c65bf54dc4f5931a57414584421865208fcba
geraldchan1 <gerald@zl-my.com>
2016-11-28 14:14:35 +0800

    Support Simditor.

--- public/js/cms/cms.js
+++ public/js/cms/cms.js
@@ -7,3 +7,33 @@ function confirmDelete() {
         alert("键入有误，删除失败。");
     }
 }
+
+$(function () {
+    var eidtor = new Simditor({
+        textarea: $('#editor'),
+        toolbar: [
+            'title',
+            'bold',
+            // 'color',
+            // 'ol',
+            // 'ul',
+            // 'blockquote',
+            'link',
+            'image',
+            'alignment',
+            // 'indent',
+            // 'outdent',
+            'hr',
+        ],
+        imageButton: 'upload',
+        defaultImage: '/storage/images/helper/photo/default.png',
+        upload : {
+            url : '/helper/upload-file',
+            params: {'_token': window.Laravel.csrfToken, 'path': $('#editor').data('path')},
+            fileKey: 'upload_file',
+            connectionCount: 3,
+            leaveConfirm: '文件上传中，您要离开页面吗？'
+        },
+    })
+
+});
