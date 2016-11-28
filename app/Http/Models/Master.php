b023317d1fc14115f9373bfa2419c153ea49a6b3
geraldchan1 <gerald@zl-my.com>
2016-11-28 14:13:52 +0800

    delete soft delete & disable increment

--- app/Http/Models/Master.php
+++ app/Http/Models/Master.php
@@ -3,15 +3,16 @@
 namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Master extends Model
 {
+    public $incrementing = false;
+
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    public function departments(){
+    public function department(){
         return $this->belongsTo('App\Http\Models\Department');
     }
 }
