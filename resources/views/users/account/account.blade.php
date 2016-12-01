200d394c95790728615193588aa9f201a3361ae7
geraldchan <gerald@zl-my.com>
2016-12-01 21:11:09 +0800

    update
    update authentication information display logic.

--- resources/views/users/account/account.blade.php
+++ resources/views/users/account/account.blade.php
@@ -9,15 +9,13 @@
         <!--图片+姓名-->
         <div class="weui-cells">
             <div class="weui-panel weui-panel_access">
-                <a href="{{isset($user)?'javascript:;':'/signin'}}" class="weui-media-box weui-media-box_appmsg">
+                <a href="{{ Auth::guest() ? '/signin' : 'javascript:;' }}" class="weui-media-box weui-media-box_appmsg">
                     <div class="weui-media-box__hd">
                         <img src="/storage/images/app/web/www/user-mobile-minepage-default-avatar.png" alt="" class="weui-media-box__thumb">
                     </div>
                     <div class="weui-media-box__bd">
-                        @if(!isset($user))
-                        <h4 class="weui-media-box__title">未登录用户</h4>
-                        <p class="weui-media-box__desc">请登陆</p>
-                        @endif
+                        <h4 class="weui-media-box__title">{{ Auth::guest() ? '未登录用户' : Auth::user()->name }}</h4>
+                        <p class="weui-media-box__desc">{{ Auth::guest() ? '请登陆' : '' }}</p>
                     </div>
                     <div class="weui-cell__ft"></div>
                 </a>
@@ -26,15 +24,15 @@
 
         <!--未登录时不添加-->
         <!--<div class="weui-cells__title">我是患者</div>-->
-        @if(isset($user))
-        <div class="weui-cells">
-            <a href="/account/user/orders/?user_id={{$user->id or ''}}" class="weui-cell weui-cell_access">
-                <div class="weui-cell__bd">
-                    <p>预约单</p>
-                </div>
-                <div class="weui-cell__ft"></div>
-            </a>
-        </div>
+        @if (!Auth::guest())
+            <div class="weui-cells">
+                <a href="/account/user/orders/?user_id={{$user->id or ''}}" class="weui-cell weui-cell_access">
+                    <div class="weui-cell__bd">
+                        <p>预约单</p>
+                    </div>
+                    <div class="weui-cell__ft"></div>
+                </a>
+            </div>
         @endif
 
         <!--常见问题等-->
@@ -61,36 +59,42 @@
             </a>
         </div>
 
-        <!--登录&注册   登录后不显示-->
-        @if(!isset($user))
-        <div class="my_mine_operate">
-            <a href="/signin" class="btnLogin">登录</a>
-            <a href="/signup" class="btnLogin my_has_before">注册</a>
-        </div>
+        <!--登录&注册 登录后不显示-->
+        @if (Auth::guest())
+            <div class="my_mine_operate">
+                <a href="/signin" class="btnLogin">登录</a>
+                <a href="/signup" class="btnLogin my_has_before">注册</a>
+            </div>
         @endif
 
         <!--退出登录  未登录时不添加-->
-        @if(isset($user))
-        <div class="my_mine_logoff">
-            <a href="/signout" class="btnLogin">退出登录</a>
-        </div>
+        @if (!Auth::guest())
+            <div class="my_mine_logoff">
+                <a href="{{ url('/signout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();" class="btnLogin">退出登录</a>
+
+                <form id="logout-form" action="{{ url('/signout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </div>
         @endif
     </div>
 
     <!--actionSheet 未登录时不添加-->
-    @if(isset($user->id))
-    <div id="actionSheet_wrap">
-        <div class="weui-mask_transparent actionsheet__mask" id="mask" style="transform-origin: 0px 0px 0px; opacity: 1; transform: scale(1, 1); display: none;"></div>
-        <div class="weui-actionsheet" id="weui-actionsheet">
-            <div class="weui-actionsheet__menu">
-                <a href="/account/user/profile/?user_id={{$user->id or ''}}" class="weui-actionsheet__cell my_order_reuploader">查看我的信息</a>
-                <a href="/account/password/reset/?user_id={{$user->id or ''}}" class="weui-actionsheet__cell my_order_delimg" style="display: block;">修改密码</a>
-            </div>
-            <div class="weui-actionsheet__action">
-                <div class="weui-actionsheet__cell" id="actionsheet_cancel" style="color: red;">取消</div>
+    @if (!Auth::guest())
+        <div id="actionSheet_wrap">
+            <div class="weui-mask_transparent actionsheet__mask" id="mask" style="transform-origin: 0px 0px 0px; opacity: 1; transform: scale(1, 1); display: none;"></div>
+            <div class="weui-actionsheet" id="weui-actionsheet">
+                <div class="weui-actionsheet__menu">
+                    <a href="/account/user/profile/?user_id={{ Auth::user()->id or ''}}" class="weui-actionsheet__cell my_order_reuploader">查看我的信息</a>
+                    <a href="/account/password/reset/?user_id={{ Auth::user()->id or ''}}" class="weui-actionsheet__cell my_order_delimg" style="display: block;">修改密码</a>
+                </div>
+                <div class="weui-actionsheet__action">
+                    <div class="weui-actionsheet__cell" id="actionsheet_cancel" style="color: red;">取消</div>
+                </div>
             </div>
         </div>
-    </div>
     @endif
 </div>
 
