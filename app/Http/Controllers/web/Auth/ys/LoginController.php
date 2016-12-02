15f142acc5914914a1b36a072baedca83d555427
geraldchan <gerald@zl-my.com>
2016-12-02 17:46:59 +0800

    first

---
+++ app/Http/Controllers/web/Auth/ys/LoginController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\web\Auth\ys;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    public function username()
+    {
+        return 'phone_number';
+    }
+
+    public function showLoginForm()
+    {
+        return view('web.auth.ys.login');
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        Validator::make($request->all(), [
+            $this->username() => 'required',
+            'password' => 'required'
+        ], [
+            $this->username() . '.required' => '手机号码不能为空。',
+            'password.required' => '密码不能为空。'
+        ])->validate();
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('doctor');
+    }
+}
---
+++ app/Http/Controllers/web/Auth/ys/ResetPasswordController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\web\Auth\ys;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Validator;
+
+class ResetPasswordController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function showResetForm(Request $request)
+    {
+        return view('web.auth.passwords.reset');
+    }
+
+    public function reset(Request $request)
+    {
+        Validator::make($request->all(), [
+            'current_password' => 'required',
+            'password' => 'required|min:6|confirmed'
+        ], [
+            'current_password.required' => '当前密码不能为空。',
+            'password.required' => '新密码不能为空。',
+            'password.min' => '新密码不能小于 6 位',
+            'password.confirmed' => '确认密码与新密码不一致。'
+        ])->validate();
+
+        $user = Auth::user();
+
+        if (Hash::check($request->current_password, $user->password)) {
+
+            $user->password = bcrypt($request->password);
+
+            $user->save();
+
+            return redirect('account');
+        } else {
+            return redirect()->back()
+                ->withErrors([
+                    'current_password' => '当前密码错误。'
+                ]);
+        }
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
