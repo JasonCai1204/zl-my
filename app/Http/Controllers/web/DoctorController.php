e4d24eacf5c6ea07249cd177526d466bf0a5b3fa
geraldchan <gerald@zl-my.com>
2016-12-02 17:47:21 +0800

    add auth:doctor middleware

--- app/Http/Controllers/web/DoctorController.php
+++ app/Http/Controllers/web/DoctorController.php
@@ -10,6 +10,10 @@ use App\Http\Controllers\Controller;
 
 class DoctorController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:doctor', ['only' => 'getOrders']);
+    }
 
     /**
     * Display a listing of the resource.
