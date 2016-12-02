bd73fbeb3be4ffcc71df0da928eb1ec192deee7f
geraldchan <gerald@zl-my.com>
2016-12-02 17:45:25 +0800

    update
    Change to extends Authenticatable for Authentication.

--- app/Doctor.php
+++ app/Doctor.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Doctor extends Model
+class Doctor extends Authenticatable
 {
     use SoftDeletes;
     protected $dates = ['deleted_at'];
