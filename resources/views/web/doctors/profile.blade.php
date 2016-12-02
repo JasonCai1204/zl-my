69ea84f3729f657fef8e092639777dd45cc82bcc
geraldchan <gerald@zl-my.com>
2016-12-02 22:24:13 +0800

    fix logout bug
    delete empty form

--- resources/views/web/doctors/profile.blade.php
+++ resources/views/web/doctors/profile.blade.php
@@ -7,16 +7,17 @@
     <link rel="stylesheet" href="/css/doctor/weui.min.css">
     <link rel="stylesheet" href="/css/doctor/home.css">
 </head>
+
 <body>
-<!--固定在屏幕顶部-->
-<div class="logo_part">
-    <div class="logo">
-        <a href="/" style="background-image: url('/storage/images/app/M_32w.png');"></a>
+    {{-- 固定在屏幕顶部 --}}
+    <div class="logo_part">
+        <div class="logo">
+            <a href="/" style="background-image: url('/storage/images/app/M_32w.png');"></a>
+        </div>
     </div>
-</div>
-<!--主体部分-->
-<div class="container" id="my_info_container">
-        <form action="">
+
+    {{-- Body --}}
+    <div class="container" id="my_info_container">
         <div class="weui-cells weui-cells_form" style="margin-top: 30px;">
             <div class="weui-cell">
                 <div class="weui-cell__hd">
@@ -47,25 +48,25 @@
                     <label class="weui-label">手机号码</label>
                 </div>
                 <div class="weui-cell__bd">
-                    <input type="number" class="weui-input" value="{{$doctor->phone_number}}" readonly />
+                    <input type="number" class="weui-input" value="{{ $doctor->phone_number }}" readonly />
                 </div>
             </div>
         </div>
-            <div class="my_mine_logoff">
-                <a href="{{ url('/logout') }}"
-                onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();" class="btnLogin">退出登录</a>
 
-                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-            </div>
-            
-            <a href="/password/reset" class="btnLink">修改密码</a>
-    </form>
-</div>
-<script type="text/javascript" src="/js/doctor/jquery-1.11.3.min.js"></script>
-<script type="text/javascript" src="/js/doctor/my_submitdisable.js"></script>
+        <div class="my_mine_logoff">
+            <a href="{{ url('/logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();" class="btnLogin">退出登录</a>
 
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        </div>
+
+        <a href="account/password/reset" class="btnLink">修改密码</a>
+    </div>
+
+    <script type="text/javascript" src="/js/doctor/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="/js/doctor/my_submitdisable.js"></script>
 </body>
 </html>
