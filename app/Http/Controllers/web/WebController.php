4ba86025d611cd2786e48bb203c8e4cf98ae3029
Jason Cai <jasoncai@hellohippies.com>
2016-12-28 09:53:44 +0800

    perfect Search function

--- app/Http/Controllers/web/WebController.php
+++ app/Http/Controllers/web/WebController.php
@@ -20,18 +20,41 @@ class WebController extends Controller
 
     public function recommend()
     {
+        $data = [];
+
         $hospitals = App\Hospital::where('is_recommended', 1)
             ->orderBy(DB::raw('CONVERT(name USING gbk)'))
             ->get();
 
+        foreach ($hospitals as $hospital) {
+            $data['hospitals'][] = [
+                'id' => $hospital->id,
+                'name' => $hospital->name,
+                'grading' => $hospital->grading,
+                'introduction' => $hospital->introduction,
+                'city' => $hospital->city->name,
+                'city_id' => $hospital->city->id,
+            ];
+        }
+
         $doctors = App\Doctor::where('is_recommended', 1)
             ->orderBy(DB::raw('CONVERT(name USING gbk)'))
+            ->select('id','name','avatar','grading','introduction','hospital_id')
             ->get();
 
-        return view('web.app.recommend', [
-            'hospitals' => $hospitals,
-            'doctors' => $doctors,
-        ]);
+        foreach ($doctors as $doctor) {
+            $data['doctors'][] = [
+                'id' => $doctor->id,
+                'name' => $doctor->name,
+                'avatar' => $doctor->avatar,
+                'grading' => $doctor->grading,
+                'introduction' => $doctor->introduction,
+                'hospital_name' => $doctor->hospital->name,
+            ];
+
+        }
+
+        return view('web.app.recommend', $data);
     }
 
 }
