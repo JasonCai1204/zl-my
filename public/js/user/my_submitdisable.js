f481529fbd1e7a6d23e4bd140b0593b60bfe0c1b
geraldchan <gerald@zl-my.com>
2016-12-30 11:40:51 +0800

    update

--- public/js/user/my_submitdisable.js
+++ public/js/user/my_submitdisable.js
@@ -8,13 +8,12 @@ $(function () {
         if($('[required]').eq(i).val() == ''){
             textflig = false;
             break;
+        }else{
+            textflig = true;
+            textflig ? $("[type = 'submit']").removeClass('btnDisable') : $("[type = 'submit']").addClass('btnDisable');
         }
     }
     $('[required]').on('input propertychange',function () {
-        if($(this).parents('div.weui-cell').hasClass('weui-cell_warn')){
-            $(this).parents('div.weui-cell').removeClass('weui-cell_warn');
-            $(this).parents('div.weui-cell').find('div.weui-cell__ft').remove();
-        }
         for(var i = 0 ;i<$('[required]').length; i++){
             if($('[required]').eq(i).val() == ''){
                 textflig = false;
@@ -23,16 +22,7 @@ $(function () {
                 textflig = true;
             }
         }
-        if(textflig == true){
-            $("[type = 'submit']").removeClass('btnDisable')
-        }else{
-            $("[type = 'submit']").addClass('btnDisable')
-        };
-
+        textflig ? $("[type = 'submit']").removeClass('btnDisable') : $("[type = 'submit']").addClass('btnDisable');
     });
-    if(textflig == true){
-        $("[type = 'submit']").removeClass('btnDisable')
-    }else{
-        $("[type = 'submit']").addClass('btnDisable')
-    }
+    textflig ? $("[type = 'submit']").removeClass('btnDisable') : $("[type = 'submit']").addClass('btnDisable');
 });
