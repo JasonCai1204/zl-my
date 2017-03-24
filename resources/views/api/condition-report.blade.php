005a8f5e4be19fce059bfa471e3d09b4a89ebd36
geraldchan <gerald@zl-my.com>
2017-03-24 11:33:53 +0800

    first

---
+++ resources/views/api/condition-report.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>病情报告 - 肿瘤名医</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+    <link rel="stylesheet" href="/css/api/app.css">
+</head>
+<body>
+    <div class="container">
+        <!-- 内容 -->
+    </div>
+</body>
+</html>
