85cc3fcaeba090377c94f51ca4dfbe81c8c2e171
JasonCai <jasoncai@zl-my.com>
2017-04-13 16:28:47 +0800

    Modify hospital detail

--- app/Http/Controllers/api/HospitalController.php
+++ app/Http/Controllers/api/HospitalController.php
@@ -15,12 +15,12 @@ class HospitalController extends Controller
 
         $hospitals = App\Hospital::orderBy(DB::raw('CONVERT(name USING gbk)'))->get();
 
-        if ($request->has('city_id')){
-            foreach ($hospitals->where('city_id', $request->city_id) as $hospital){
+        if ($request->has('city_id')) {
+            foreach ($hospitals->where('city_id', $request->city_id) as $hospital) {
                 $data['hospitals'][] = [
                     'id' => $hospital->id,
                     'name' => $hospital->name,
-                    'avatar' => count($hospital->avatar) > 0 ? $hospital->avatar :'images/hospital/avatar/default.png',
+                    'avatar' => count($hospital->avatar) > 0 ? $hospital->avatar : 'images/hospital/avatar/default.png',
                     'grading' => $hospital->grading,
                     'city_id' => $hospital->city->id,
                     'city_name' => $hospital->city->name,
@@ -30,7 +30,7 @@ class HospitalController extends Controller
             return collect([
                 'status' => 1,
                 'msg' => '加载成功',
-                'data' => isset($data['hospitals']) && count($data['hospitals']) >0 ? $data : ['hospitals' => $data['hospitals'] = []]
+                'data' => isset($data['hospitals']) && count($data['hospitals']) > 0 ? $data : ['hospitals' => $data['hospitals'] = []]
             ]);
 
         }
@@ -39,7 +39,7 @@ class HospitalController extends Controller
             $data['hospitals'][] = [
                 'id' => $hospital->id,
                 'name' => $hospital->name,
-                'avatar' => count($hospital->avatar) > 0 ? $hospital->avatar :'images/hospital/avatar/default.png',
+                'avatar' => count($hospital->avatar) > 0 ? $hospital->avatar : 'images/hospital/avatar/default.png',
                 'grading' => $hospital->grading,
                 'city_id' => $hospital->city->id,
                 'city_name' => $hospital->city->name,
@@ -49,8 +49,8 @@ class HospitalController extends Controller
 
         $cities = App\City::orderBy(DB::raw('CONVERT(name USING gbk)'))->get();
 
-        foreach ($cities as $city){
-            $data['cities'][] =[
+        foreach ($cities as $city) {
+            $data['cities'][] = [
                 'id' => $city->id,
                 'name' => $city->name
             ];
@@ -68,40 +68,52 @@ class HospitalController extends Controller
     public function detail(Request $request)
     {
         $hospital = App\Hospital::findOrFail($request->hospital_id);
+        $h_doctors = $hospital->doctors()->orderBy(DB::raw('CONVERT(name USING gbk)'))->get();
 
-        foreach ($hospital->doctors()->orderBy(DB::raw('CONVERT(name USING gbk)'))->get() as $doctor ) {
-            $info1 = [
-                'id' => $doctor->id,
-                'avatar' => count($doctor->avatar) > 0 ? $doctor->avatar :'images/doctor/avatar/default.png',
-                'name' => $doctor->name,
-                'grading' => $doctor->grading,
-                'hospital_id' => $doctor->hospital_id,
-                'is_certified' => $doctor->is_certified,
-            ];
+        if (count($h_doctors) > 0) {
+            foreach ($h_doctors as $doctor) {
+                $info1 = [
+                    'id' => $doctor->id,
+                    'avatar' => count($doctor->avatar) > 0 ? $doctor->avatar : 'images/doctor/avatar/default.png',
+                    'name' => $doctor->name,
+                    'grading' => $doctor->grading,
+                    'hospital_id' => $doctor->hospital_id,
+                    'is_certified' => $doctor->is_certified,
+                ];
 
+                $instances = [];
 
-            $instances = [];
-
-                foreach ($doctor->instances as $instance){
-                    $instances[] =[
+                foreach ($doctor->instances as $instance) {
+                    $instances[] = [
                         'id' => $instance->id,
                         'name' => $instance->name
-                    ] ;
+                    ];
 
                 }
 
-            $doctors['doctors'][] = array_merge($info1, ['instances' => $instances]);
+                $doctors['doctors'][] = array_merge($info1, ['instances' => $instances]);
+            }
         }
 
+        if(count($h_doctors) > 0) {
+            return collect([
+                'status' => 1,
+                'msg' => '加载成功',
+                'data' => [
+                    'doctors' => $doctors['doctors'],
+                    'introduction' => $hospital->introduction
+                ]
+            ])->toJson();
+        }
 
         return collect([
             'status' => 1,
             'msg' => '加载成功',
             'data' => [
-                'doctors' => $doctors['doctors'],
                 'introduction' => $hospital->introduction
             ]
         ])->toJson();
+
     }
 
     // Search hospitals and doctors
@@ -120,10 +132,10 @@ class HospitalController extends Controller
 
         // Search hospital or doctor.
         $hospitals = App\Hospital::where('name', 'like', '%' . $request->q . '%')
-            ->select('id', 'avatar','name', 'grading', 'is_recommended', 'city_id')
+            ->select('id', 'avatar', 'name', 'grading', 'is_recommended', 'city_id')
             ->get();
 
-        if (isset($hospitals) && count($hospitals)>0) {
+        if (isset($hospitals) && count($hospitals) > 0) {
             foreach ($hospitals as $hospital) {
                 $data['hospitals'][] = [
                     'id' => $hospital->id,
@@ -133,7 +145,7 @@ class HospitalController extends Controller
                     'city_name' => $hospital->city->name
                 ];
             }
-        }else {
+        } else {
             $data['hospitals'] = [];
         }
 
@@ -142,12 +154,12 @@ class HospitalController extends Controller
             ->select('id', 'avatar', 'name', 'grading', 'hospital_id', 'is_certified', 'is_recommended')
             ->get();
 
-        if (isset($doctors) && count($doctors)>0){
+        if (isset($doctors) && count($doctors) > 0) {
 
             foreach ($doctors as $doctor) {
                 $info1 = [
                     'id' => $doctor->id,
-                    'avatar' => count($doctor->avatar) > 0 ? $doctor->avatar :'images/doctor/avatar/default.png',
+                    'avatar' => count($doctor->avatar) > 0 ? $doctor->avatar : 'images/doctor/avatar/default.png',
                     'name' => $doctor->name,
                     'grading' => $doctor->grading,
                     'hospital_id' => $doctor->hospital_id,
@@ -168,7 +180,7 @@ class HospitalController extends Controller
 
 
             }
-        } else{
+        } else {
             $data['doctors'] = [];
         }
 
