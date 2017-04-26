4117788578e45203a73ac7cc5a45295057a72d92
JasonCai <jasoncai@zl-my.com>
2017-04-26 10:52:22 +0800

    Migrtions
    - reviews  add;
    - patients add $table->string('avatar', 225)->nullable()->after('id’).

---
+++ database/migrations/2017_04_26_103207_add_avatar_to_patients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarToPatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            $table->string('avatar', 225)->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            //
+        });
+    }
+}
