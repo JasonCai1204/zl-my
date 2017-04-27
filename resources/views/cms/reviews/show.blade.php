e298f1661863f488b517a33e389d699054c28c76
geraldchan <geraldchan@hippy.cc>
2017-04-27 18:55:07 +0800

    Update title and urls, read only inputs and remove delete button.

--- resources/views/cms/reviews/show.blade.php
+++ resources/views/cms/reviews/show.blade.php
@@ -1,36 +1,63 @@
 @extends('cms.layouts.app')
 
-@section('title', '城市详情')
+@section('title', '评论详情')
 
 @section('content')
     <div class="container">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">城市详情</div>
+                <div class="panel-heading">评论详情</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/cities/' . $data->id) }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('reviews/' . $data->id) }}">
                         {{ csrf_field() }}
                         {{ method_field('PUT') }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">名称*</label>
+                        <div class="form-group">
+                            <label for="patient_id" class="col-md-4 control-label">评论者*</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') ?: $data->name }}" required>
+                                <input class="form-control" name="patient_id" value="{{ $data->patient->name }}" readonly>
+                            </div>
+                        </div>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="form-group">
+                            <label for="doctor_id" class="col-md-4 control-label">医生*</label>
+
+                            <div class="col-md-6">
+                                <input class="form-control" name="doctor_id" value="{{ $data->doctor->name }}" readonly>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="ratings" class="col-md-4 control-label">评级*</label>
+
+                            <div class="col-md-6">
+                                <input class="form-control" name="ratings" value="{{ $data->ratings }}" readonly>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="reviews" class="col-md-4 control-label">评论*</label>
+
+                            <div class="col-md-6">
+                                <textarea name="reviews" rows="12" class="form-control" readonly>{{ $data->reviews }}</textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="status" value="1"{{ $data->status === 1 ? ' checked' : '' }}> 通过审核
+                                    </label>
+                                </div>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">保存</button>
-                                <a class="btn btn-link" href="javascript:;" onclick="confirmDelete()">删除</a>
                             </div>
                         </div>
                     </form>
